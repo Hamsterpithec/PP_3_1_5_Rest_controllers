0b5d2da7a79d51411952bb03bd23f70b6ce3e4ee
Alex <locher91@mail.ru>
2024-04-23 19:46:46 +0300

    Auth without rolls

--- src/main/java/PP_3_1_2_Spring_security/config/SecurityConfig.java
+++ src/main/java/PP_3_1_2_Spring_security/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package PP_3_1_2_Spring_security.config;
 
-import PP_3_1_2_Spring_security.security.AuthProviderImpl;
+import PP_3_1_2_Spring_security.service.PersonDetailsService;
+
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
@@ -8,15 +9,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfiguration {
-    private final AuthProviderImpl authProvider;
 
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    private final PersonDetailsService personDetailsService;
+
+
+
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
-
 
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        auth.authenticationProvider(authProvider);
+        auth.userDetailsService(personDetailsService);
     }
 }
