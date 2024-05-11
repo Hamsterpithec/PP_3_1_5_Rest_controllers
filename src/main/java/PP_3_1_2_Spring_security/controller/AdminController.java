b19fc6a4b7222824b2048eedcdfdb7a3f30e7695
Alex <locher91@mail.ru>
2024-05-11 19:09:37 +0300

    11.05

--- src/main/java/PP_3_1_2_Spring_security/controller/AdminController.java
+++ src/main/java/PP_3_1_2_Spring_security/controller/AdminController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.ArrayList;
-import java.util.List;
+
 
 @Controller
 @RequestMapping("/admin")
